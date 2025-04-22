bfb16210e71c2f49b1d7ca340012be0240063f8b
satyajit <satyajit_jbl@yahoo.com>
2025-04-22 13:06:19 +0600

    if and switch

--- day4/index.js
+++ day4/index.js
@@ -1,1 +1,23 @@
-console.log("day 04 in js")
+// let number = 40;
+
+// if (number>60){
+//     console.log("Aged")
+// }
+// else if (number >90){
+//     console.log("okd")
+// }
+// else {
+//     console.log("young")
+// }
+console.log(new Date().getDay())
+switch(new Date().getDay()){
+    case 0: day = "sunday"; break;
+    case 1: day = "MOnday"; break;
+    case 2: day = "Tuesday"; break;
+    case 3: day = "Wednesday"; break;
+    case 4: day = "Thursday"; break;
+    case 5: day = "Friday"; break;
+    case 6: day = "Saturday"; break;
+    
+}
+console.log(day)
